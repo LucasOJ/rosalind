d6485d2f81e589aa8961f1165e4a36580b6cb43e
LucasOJ <lucasodowdjones@gmail.com>
2024-05-07 17:48:16 +0100

    Add comments to problem 1E

--- chapter_1/problem_e.py
+++ chapter_1/problem_e.py
@@ -1,6 +1,10 @@
 # https://rosalind.info/problems/ba1e/
 
-def find_clumped_kmers(genome: str, k: int, L: int, t: int) -> list[str]:
+from typing import List
+
+# Finding all k-mer positions, and indentifying which correspond to (L, t)-clumps
+
+def find_clumped_kmers(genome: str, k: int, L: int, t: int) -> List[str]:
     kmer_map = {}
     for i in range(len(genome) - k):
         kmer = genome[i:i+k]
@@ -8,15 +12,18 @@ def find_clumped_kmers(genome: str, k: int, L: int, t: int) -> list[str]:
             kmer_map[kmer] = []
         kmer_map[kmer].append(i)
 
-    print(kmer_map)
+    # kmer_map is large: The keys alone are O(|genome|k)
+    # Could make smaller by storing just the index of the first location
 
+    # Parallelisable with map-reduce
     clumped_kmers = [kmer for (kmer, locs) in kmer_map.items() if is_clump(locs, k, L, t)]
 
     return clumped_kmers
             
+# Gets more expensive the more k-mer repetitions we see
+def is_clump(locs: List[int], k: int, L: int, t: int) -> bool:
 
-def is_clump(locs: list[int], k: int, L: int, t: int) -> bool:
-
+    # Sliding window of t adjacent k-merk positions
     for i in range(len(locs) - t + 1):
         # Add k because locs[i] is the start position of the i th k-mer
         t_interval_end = locs[i + (t-1)] + k 
@@ -28,3 +35,7 @@ def is_clump(locs: list[int], k: int, L: int, t: int) -> bool:
             return True
 
     return False
+
+# Keeping track of k-mer frequencies in a sliding L-window
+
+# TODO
